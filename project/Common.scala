468c02ff1e7f6117ab3ffc018084000ab0fc28ab
Zoltán Balázs <balagez@gmail.com>
2018-06-28 14:35:31 -0400

    Scala 2.12 support

--- project/Common.scala
+++ project/Common.scala
@@ -1,6 +1,6 @@
 object Common {
-  def version = "1.1.2"  
-  def playVersion = System.getProperty("play.version", "2.4.2")
-  def scalaVersion =  "2.11.5"
-  def scalaVersions =  Seq("2.10.4", scalaVersion)
+  def version = "1.1.2-kinja"  
+  def playVersion = System.getProperty("play.version", "2.6.13")
+  def scalaVersion =  "2.12.6"
+  def scalaVersions =  Seq("2.11.8", scalaVersion)
 }
