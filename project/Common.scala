1404bacec9675aac4a251b127ecfada7839a848b
szilard <szilard@kinja.com>
2019-03-26 14:13:37 +0100

    Fixing unwanted versioning
    BUG:
    If a cloudinary id contains slash like "qa/3655675" then during the url generation a default version `v1` generated into the url
    which can lead an unwanted version of the image:
    https://i.kinja-img.com/gawker-media/image/upload/s--f8cjIVdR--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/v1/qa/3655675.jpg
    vs
    https://i.kinja-img.com/gawker-media/image/upload/s--f8cjIVdR--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/qa/3655675.jpg
    This can happen if a video thumbnail is updated in MCP, the id stays the same but the content is updated.
    
    Asana Ticket:
    https://app.asana.com/0/419456048135282/1115870584175636

--- project/Common.scala
+++ project/Common.scala
@@ -1,5 +1,5 @@
 object Common {
-  def version = "1.1.2-kinja"  
+  def version = "1.1.3-kinja"
   def playVersion = System.getProperty("play.version", "2.6.13")
   def scalaVersion =  "2.12.6"
   def scalaVersions =  Seq("2.11.8", scalaVersion)
